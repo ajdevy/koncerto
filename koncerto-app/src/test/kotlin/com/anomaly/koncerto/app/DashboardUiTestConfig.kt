9e02e331b81e9bd84570252593edad376a7d5651
AJ <ajdev@yandex.com>
2026-06-14 09:21:33 +0300

    fix: add missing Profile import

--- koncerto-app/src/test/kotlin/com/anomaly/koncerto/app/DashboardUiTestConfig.kt
+++ koncerto-app/src/test/kotlin/com/anomaly/koncerto/app/DashboardUiTestConfig.kt
@@ -15,6 +15,7 @@ import java.nio.file.Paths
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 
 @Profile("ui-test")
 @TestConfiguration(proxyBeanMethods = false)
